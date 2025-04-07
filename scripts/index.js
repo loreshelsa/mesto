5ce65c7df80e5137e5a4310fdfa370b20223a1d1
Elza Loresh <lorec-and@yandex.ru>
2025-04-07 13:41:32 +0300

    added methods for drawing and removing cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
+
+function renderCard(card, remove) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = card.link;
+  cardImage.setAttribute("alt", card.name);
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  const deleteBtn = cardElement.querySelector(".card__delete-button");
+  deleteBtn.addEventListener("click", () => remove(cardElement));
+  
+  return cardElement;
+}
+
+function removeCard(cardElement) {
+  cardElement.remove();
+}
+
+function renderList() {
+  const listCards = initialCards.map((card) => renderCard(card, removeCard));
+  cardContainer.append(...listCards);
+}
+
+document.addEventListener("DOMContentLoaded", renderList);
