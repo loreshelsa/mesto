5f6cb12dc0b58f08ad36046284ade10fd77bd023
Elza Loresh <lorec-and@yandex.ru>
2025-05-14 14:51:38 +0300

    added opening with animation, additing new cards, clearing form, editing profile info, close popup to overlay

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-import './index.css'; 
-import {initialCards} from './../scripts/cards';
-
-const avatarImage = new URL('./../images/avatar.jpg', import.meta.url);
-const cardOne = new URL('./../images/card_1.jpg', import.meta.url);
-const cardTwo = new URL('./../images/card_2.jpg', import.meta.url);
-const cardThree = new URL('./../images/card_3.jpg', import.meta.url);
-
-
-const pictures = [
-  // меняем исходные пути на переменные
-  { name: 'avatar', link: avatarImage },
-  { name: 'card-1', link: cardOne },
-  { name: 'card-2', link: cardTwo },
-  { name: 'card-3', link: cardThree },
-]; 
-
-
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+import "./index.css";
+import { initialCards } from "./../scripts/cards";
 
 const cardTemplate = document.querySelector("#card-template").content;
 const cardContainer = document.querySelector(".places__list");
@@ -42,7 +17,15 @@ const previewPopupCloseButton = previewPopup.querySelector(".popup__close");
 const popupImage = previewPopup.querySelector(".popup__image");
 const popupDescription = previewPopup.querySelector(".popup__caption");
 
-function renderCard(card, remove) {
+let activePopup = null;
+
+function addAnimationPopups() {
+  editPopup.classList.add("popup_is-animated");
+  previewPopup.classList.add("popup_is-animated");
+  newCardPopup.classList.add("popup_is-animated");
+}
+
+function renderCard(card, remove, like) {
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
@@ -57,13 +40,16 @@ function renderCard(card, remove) {
     remove(cardElement);
   });
 
-  cardElement.addEventListener("click", () => openPreview(card));
+  cardElement.addEventListener("click", (event) => {
+    event.stopPropagation();
+    openPreview(card);
+  });
 
   const likeBtn = cardElement.querySelector(".card__like-button");
   likeBtn.addEventListener("click", (event) => {
     event.stopPropagation();
-    likeBtn.classList.add("card__like-button_is-active");
-  })
+    like(likeBtn);
+  });
 
   return cardElement;
 }
@@ -72,17 +58,38 @@ function removeCard(cardElement) {
   cardElement.remove();
 }
 
+function toggleLike(likeBtn) {
+  likeBtn.classList.toggle("card__like-button_is-active");
+}
+
 function renderList() {
-  const listCards = initialCards.map((card) => renderCard(card, removeCard));
+  const listCards = initialCards.map((card) =>
+    renderCard(card, removeCard, toggleLike)
+  );
   cardContainer.append(...listCards);
 }
 
 function openPopup(popup) {
-  popup.classList.add("popup_is-animated");
+  activePopup = popup;
   popup.classList.add("popup_is-opened");
+
+  //закрытие по оверлей
+  document.addEventListener("click", closePopupOverlay);
+}
+
+function closePopupOverlay(event) {
+  const popupContent = activePopup.querySelector(".popup__content");
+  if (
+    event.target !== popupContent &&
+    event.target.closest(".popup__content") !== popupContent
+  ) {
+    closePopup(activePopup);
+    document.removeEventListener("click", closePopupOverlay);
+  }
 }
 
 function closePopup(popup) {
+  activePopup = null;
   popup.classList.remove("popup_is-opened");
 }
 
@@ -95,14 +102,77 @@ function openPreview(card) {
 
 document.addEventListener("DOMContentLoaded", renderList);
 
-editButton.addEventListener("click", () => openPopup(editPopup));
-editPopupCloseButton.addEventListener("click", () => closePopup(editPopup));
+editButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  openEditPopup();
+});
+editPopupCloseButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  closePopup(editPopup);
+});
 
-addCardButton.addEventListener("click", () => openPopup(newCardPopup));
-newCardPopupCloseButton.addEventListener("click", () =>
-  closePopup(newCardPopup)
-);
+//закрытие по кнопке escape
+document.addEventListener("keydown", function (event) {
+  if (event.key?.toLowerCase() === "escape") {
+    closePopup(activePopup);
+  }
+});
 
-previewPopupCloseButton.addEventListener("click", () =>
-  closePopup(previewPopup)
-);
+addCardButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  openPopup(newCardPopup);
+});
+newCardPopupCloseButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  closePopup(newCardPopup);
+});
+
+previewPopupCloseButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  closePopup(previewPopup);
+});
+
+//Редактирование имени и информации о себе по кнопке сохранить
+const formElement = document.forms["edit-profile"];
+const inputName = formElement.elements["name"];
+const inputDescription = formElement.elements["description"];
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+function openEditPopup() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+  openPopup(editPopup);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+
+  closePopup(editPopup);
+}
+formElement.addEventListener("submit", handleFormSubmit);
+
+//Добавление карточки с картинкой по кнопке сохранить
+const newPlaceForm = document.forms["new-place"];
+const inputNameCard = newPlaceForm.elements["place-name"];
+const inputCardLink = newPlaceForm.elements["link"];
+
+function clearNewCardForm() {
+  inputNameCard.value = "";
+  inputCardLink.value = "";
+}
+
+function addNewCard(event) {
+  event.preventDefault();
+  const newCard = { name: inputNameCard.value, link: inputCardLink.value };
+  const card = renderCard(newCard, removeCard, toggleLike);
+  cardContainer.prepend(card);
+  closePopup(activePopup);
+  clearNewCardForm();
+}
+newPlaceForm.addEventListener("submit", addNewCard);
+
+addAnimationPopups();
